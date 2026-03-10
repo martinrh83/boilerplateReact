633eb3efadd2ef4fc85121e97e769b6df48bbc5d
martinrh83 <martinrh83@gmail.com>
2026-03-10 18:49:15 -0300

    feat: add Husky pre-commit hook for linting and update package dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState();
   return (
     <>
       <h1>Vite + React</h1>
